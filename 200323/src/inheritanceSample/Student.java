c86d95ad83eea0c77163bb7ba9ccc2c73c51ea97
kej388 <eunjinkoh96@gmail.com>
2020-03-23 14:00:12 +0900

    오전 수업 상속학습

---
+++ 200323/src/inheritanceSample/Student.java
@@ -0,0 +1,7 @@
+package inheritanceSample;
+
+public class Student extends Person{
+	public void study() {
+		System.out.println("공부하기");
+	}
+}
---
+++ 200323/src/inheritanceSample/StudentWorker.java
@@ -0,0 +1,7 @@
+package inheritanceSample;
+
+public class StudentWorker extends Student{
+	public void work() {
+		System.out.println("일하기");
+	}
+}
